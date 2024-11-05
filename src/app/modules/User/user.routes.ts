21d0e8bf536ab571090a5a9a45c3d5d8f6f8d21a
ibnabdullah1 <arfathossen541@gmail.com>
2024-11-05 10:37:41 +0600

    Initial commit

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { userController } from './user.controller';
 
 const router = express.Router();
 
-router.post("/", userController.createAdmin);
+router.post("/create-admin", userController.createAdmin);
 
 export const userRoutes = router;
