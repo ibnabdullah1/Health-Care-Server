637346b316fed9269a180fbacba85fa6c0b71132
ibnabdullah1 <arfathossen541@gmail.com>
2024-11-05 10:40:26 +0600

    initial comm

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,13 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import router from './app/routes';
-import httpStatus from 'http-status';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import cookieParser from 'cookie-parser';
-import { AppointmentService } from './app/modules/Appointment/appointment.service';
-import cron from 'node-cron'
+import { userRoutes } from './app/modules/User/user.routes';
 
 const app: Application = express();
 app.use(cors());
-app.use(cookieParser());
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-
-cron.schedule('* * * * *', () => {
-    try {
-        AppointmentService.cancelUnpaidAppointments();
-    }
-    catch (err) {
-        console.error(err);
-    }
-});
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
@@ -32,19 +15,6 @@ app.get('/', (req: Request, res: Response) => {
     })
 });
 
-app.use('/api/v1', router);
-
-app.use(globalErrorHandler);
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(httpStatus.NOT_FOUND).json({
-        success: false,
-        message: "API NOT FOUND!",
-        error: {
-            path: req.originalUrl,
-            message: "Your requested path is not found!"
-        }
-    })
-})
+app.use('/api/v1/user', userRoutes);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,12 @@
 import { Server } from 'http';
 import app from './app'
-import config from './config';
 
-
+const port = 3000;
 
 async function main() {
-    const server: Server = app.listen(config.port, () => {
-        console.log("Sever is running on port ", config.port);
-    });
-
-    const exitHandler = () => {
-        if (server) {
-            server.close(() => {
-                console.info("Server closed!")
-            })
-        }
-        process.exit(1);
-    };
-    process.on('uncaughtException', (error) => {
-        console.log(error);
-        exitHandler();
-    });
-
-    process.on('unhandledRejection', (error) => {
-        console.log(error);
-        exitHandler();
+    const server: Server = app.listen(port, () => {
+        console.log("Sever is running on port ", port);
     })
-};
+}
 
 main();
